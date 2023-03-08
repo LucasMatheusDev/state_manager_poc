6edd09f9c3ad7d94b621994fbaf7e5cf10c613e7
LucasMatheusDev <lucasmatheusdev@gmail.com>
2023-03-08 17:40:52 -0300

    create data

---
+++ lib/modules/home/model/data/home_data_source.dart
@@ -0,0 +1,73 @@
+import 'package:faker/faker.dart';
+import 'package:state_manager_poc/modules/home/model/entities/friend.dart';
+import 'package:state_manager_poc/modules/home/model/entities/post.dart';
+import 'package:state_manager_poc/modules/home/model/exceptions/rest_client_exception.dart';
+
+class HomeDataSource {
+  Future<List<Post>> getPosts() async {
+    final probabilityResponses = [
+      'success',
+      'error',
+      'success',
+      'notFound',
+      'success'
+    ];
+    probabilityResponses.shuffle();
+    final String response = probabilityResponses.first;
+    await Future.delayed(const Duration(seconds: 2));
+
+    if (response == 'success') {
+      return List.generate(
+        10,
+        (index) => Post(
+          title: faker.lorem.sentence(),
+          description: faker.lorem.sentence(),
+          imageUrl: 'https://picsum.photos/200/300?random=$index',
+        ),
+      );
+    } else if (response == 'notFound') {
+      throw RestClientException(
+        message: 'Not Found posts',
+        code: 404,
+      );
+    } else {
+      throw RestClientException(
+        message: 'Internal Server Error',
+        code: 500,
+      );
+    }
+  }
+
+  Future<List<Friend>> getFriends() async {
+    final probabilityResponses = [
+      'success',
+      'error',
+      'success',
+      'notFound',
+      'success'
+    ];
+    probabilityResponses.shuffle();
+    final String response = probabilityResponses.first;
+    await Future.delayed(const Duration(seconds: 2));
+
+    if (response == 'success') {
+      return List.generate(
+        10,
+        (index) => Friend(
+          name: faker.person.name(),
+          imageUrl: 'https://picsum.photos/200/300?random=$index',
+        ),
+      );
+    } else if (response == 'notFound') {
+      throw RestClientException(
+        message: 'Not Found friends',
+        code: 404,
+      );
+    } else {
+      throw RestClientException(
+        message: 'Internal Server Error',
+        code: 500,
+      );
+    }
+  }
+}
---
+++ lib/modules/home/model/entities/post.dart
@@ -0,0 +1,11 @@
+class Post{
+  final String title;
+  final String description;
+  final String imageUrl;
+
+  Post({
+    required this.title,
+    required this.description,
+    required this.imageUrl,
+  });
+}
