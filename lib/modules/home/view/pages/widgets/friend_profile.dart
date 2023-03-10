17ebdf0de243b70953958cef84058a2ad345cea6
LucasMatheusDev <lucasmatheusdev@gmail.com>
2023-03-10 14:18:33 -0300

    up friend page

--- lib/modules/home/view/pages/widgets/friend_profile.dart
+++ lib/modules/home/view/pages/widgets/friend_profile.dart
@@ -11,51 +11,60 @@ class FriendProfile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
+      physics: const BouncingScrollPhysics(),
       shrinkWrap: true,
       itemCount: friends.length,
       scrollDirection: Axis.horizontal,
+      padding: const EdgeInsets.all(0),
       itemBuilder: (context, index) {
         return Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Container(
-                height: 80,
-                width: 80,
-                decoration: BoxDecoration(
-                  color: Colors.grey,
-                  borderRadius: BorderRadius.circular(50),
-                  image: DecorationImage(
-                    image: NetworkImage(friends[index].imageUrl),
-                    fit: BoxFit.cover,
+          padding: const EdgeInsets.symmetric(horizontal: 4),
+          child: SizedBox(
+            height: 90,
+            width: 90,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Container(
+                  height: 70,
+                  width: 70,
+                  decoration: BoxDecoration(
+                    color: Colors.grey,
+                    borderRadius: BorderRadius.circular(50),
+                    image: DecorationImage(
+                      image: NetworkImage(friends[index].imageUrl),
+                      fit: BoxFit.cover,
+                    ),
                   ),
-                ),
-                child: Stack(
-                  children: [
-                    Positioned(
-                      bottom: 0,
-                      right: 0,
-                      child: Container(
-                        height: 20,
-                        width: 20,
-                        decoration: BoxDecoration(
-                          color: Colors.green,
-                          borderRadius: BorderRadius.circular(50),
+                  child: Stack(
+                    children: [
+                      Positioned(
+                        bottom: 0,
+                        right: 0,
+                        child: Container(
+                          height: 20,
+                          width: 20,
+                          decoration: BoxDecoration(
+                            color: Colors.green,
+                            borderRadius: BorderRadius.circular(50),
+                          ),
                         ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
-              FittedBox(
-                child: Text(
-                  friends[index].name,
-                  textAlign: TextAlign.center,
-                  style: Theme.of(context).textTheme.bodyText1,
+                FittedBox(
+                  fit: BoxFit.fitWidth,
+                  child: Text(
+                    friends[index].name,
+                    textAlign: TextAlign.center,
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                    style: Theme.of(context).textTheme.bodyText1,
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         );
       },
