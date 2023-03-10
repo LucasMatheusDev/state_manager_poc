b5b085d059a24517ede4b09c034b1a7865c2867b
LucasMatheusDev <lucasmatheusdev@gmail.com>
2023-03-10 15:29:45 -0300

    chore: remove unused code

--- lib/modules/home/view/states/state_managers/account_views_state.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/cupertino.dart';
-import 'package:state_manager_poc/modules/home/view/states/account_views_state.dart';
-
-class AccountViewsStateManager {
-  final accountViews = ValueNotifier<AccountViewsState>(AccountViewsInitial());
-}
